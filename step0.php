8154d453716e2aee650c1d29b08c71321460fcc8
angelica.marie <apilado.314480@calamba.sti.edu.ph>
2024-12-06 22:13:15 +0800

    update 12/06/2024

---
+++ step0.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>STI COLLEGE</title>
+
+    <!-- LINKS -->
+    <link rel="stylesheet" href="./CSS/index.css">
+</head>
+<body>
+    <div class="container">
+        <div class="heads">
+            <div class="icon">
+                <div class="icon-img"><img src="./ASSET/sti-logo.png" alt=""></div>
+            </div>
+            <div class="buttons">
+                <h2 style="color: #fefefe; font-family: 'Poppins', sans-serif;">Online Application</h2>
+            </div>
+        </div>
+
+        
+        <div class="grid-container">
+            <div class="grid-wrapper">
+                <div class="grid-items"><div class="item1"><img src="./ASSET/ACT.jpg" alt=""><div class="overlay"><h1>Associate in Computer Technology</h1></div></div></div>
+                <div class="grid-items"><div class="item2"><img src="./ASSET/BACOMM.jpg" alt=""><div class="overlay"><h1>Bachelor of Arts in Communication</h1></div></div></div>
+                <div class="grid-items"><div class="item3"><img src="./ASSET/BMMA.jpg" alt=""><div class="overlay"><h1>Bachelor of Multimedia Arts</h1></div></div></div>
+            </div>
+        </div>
+    </div>
+
+    <script src="./JS/function.js"></script>
+</body>
+</html>
